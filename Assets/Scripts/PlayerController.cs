8770586aebeb3aaec248e7992617163911518d72
joshxee <jcle919@aucklanduni.ac.nz>
2017-10-03 10:58:02 +1300

    Someone say that this worked

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+}
